b5d42ed90952d93a03d0c366800ce645e9b86a08
Ramazan İLTER <remzilter@gmail.com>
2022-11-02 02:54:28 +0300

    add validate-int

---
+++ complete-filters-php-script/validate-integer.php
@@ -0,0 +1,17 @@
+
+<!doctype html>
+<html>
+<body>
+
+<?php
+$int = 100;
+
+if (!filter_var($int, FILTER_VALIDATE_INT) === false) {
+  echo("Integer is valid");
+} else {
+  echo("Integer is not valid");
+}
+?>
+
+</body>
+</html>
