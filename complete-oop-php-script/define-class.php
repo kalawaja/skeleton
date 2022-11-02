6fcd33293f8bba7626d0201be1fa7292ab6bc077
Ramazan İLTER <remzilter@gmail.com>
2022-11-03 02:17:10 +0300

    add define-class

---
+++ complete-oop-php-script/define-class.php
@@ -0,0 +1,18 @@
+
+<?php
+class Fruit {
+  // Properties
+  public $name;
+  public $color;
+
+  // Methods
+  function set_name($name) {
+    $this->name = $name;
+  }
+  function get_name() {
+    return $this->name;
+  }
+}
+?>
+
+<!-- Note: In a class, variables are called properties and functions are called methods! -->
