c035a3b8d78e0c3b3bbb440ca5f174ff9f021d4a
Ramazan İLTER <remzilter@gmail.com>
2022-11-03 02:32:19 +0300

    add instanceof

---
+++ complete-oop-php-script/instanceof.php
@@ -0,0 +1,28 @@
+
+<!-- You can use the instanceof keyword to check if an object belongs to a specific class: -->
+
+<!doctype html>
+<html>
+<body>
+
+<?php
+class Fruit {
+  // Properties
+  public $name;
+  public $color;
+
+  // Methods
+  function set_name($name) {
+    $this->name = $name;
+  }
+  function get_name() {
+    return $this->name;
+  }
+}
+
+$apple = new Fruit();
+var_dump($apple instanceof Fruit);
+?>
+ 
+</body>
+</html>
