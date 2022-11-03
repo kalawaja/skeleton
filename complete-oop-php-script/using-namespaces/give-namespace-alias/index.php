26357d97ee777bf964ec19a8ac3aaeb7463d37ca
Ramazan İLTER <remzilter@gmail.com>
2022-11-04 02:00:36 +0300

    add give-namespace-alias with 'use' keyword

---
+++ complete-oop-php-script/using-namespaces/give-namespace-alias/index.php
@@ -0,0 +1,16 @@
+
+<?php
+include "Html.php";
+use Html as H;
+$table = new H\Table();
+$table->title = "My table";
+$table->numRows = 5;
+?>
+
+<html>
+<body>
+
+<?php $table->message(); ?>
+
+</body>
+</html>
