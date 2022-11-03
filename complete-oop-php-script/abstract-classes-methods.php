343eeb53ff01e103460c2fb6566e7f9a8b6fb053
Ramazan İLTER <remzilter@gmail.com>
2022-11-03 06:20:59 +0300

    add abstract class-method and more abstract...

--- complete-oop-php-script/abstract-classes-methods.php
+++ complete-oop-php-script/abstract-classes-methods.php
@@ -55,3 +55,74 @@ echo $citroen->intro();
 </html>
 
 <!-- Note: intro() is an abstract method that should be defined in all the child classes and they should return a string. -->
+
+<!-- More Abstract Class Example 1 -->
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+abstract class ParentClass {
+  // Abstract method with an argument
+  abstract protected function prefixName($name);
+}
+
+class ChildClass extends ParentClass {
+  public function prefixName($name) {
+    if ($name == "John Doe") {
+      $prefix = "Mr.";
+    } elseif ($name == "Jane Doe") {
+      $prefix = "Mrs.";
+    } else {
+      $prefix = "";
+    }
+    return "{$prefix} {$name}";
+  }
+}
+
+$class = new ChildClass;
+echo $class->prefixName("John Doe");
+echo "<br>"; 
+echo $class->prefixName("Jane Doe");
+?>
+ 
+</body>
+</html>
+
+<!-- More Abstract Class Example 2 -->
+
+<!doctype html>
+<html>
+<body>
+
+<?php
+/*
+abstract class ParentClass {
+  // Abstract method with an argument
+  abstract protected function prefixName($name);
+}
+
+class ChildClass extends ParentClass {
+  // The child class may define optional arguments that is not in the parent's abstract method
+  public function prefixName($name, $separator = ".", $greet = "Dear") {
+    if ($name == "John Doe") {
+      $prefix = "Mr";
+    } elseif ($name == "Jane Doe") {
+      $prefix = "Mrs";
+    } else {
+      $prefix = "";
+    }
+    return "{$greet} {$prefix}{$separator} {$name}";
+  }
+}
+
+$class = new ChildClass;
+echo $class->prefixName("John Doe");
+echo "<br>";  
+echo $class->prefixName("Jane Doe");
+*/
+?>
+ 
+</body>
+</html>
