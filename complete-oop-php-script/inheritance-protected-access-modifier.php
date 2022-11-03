88f52f911c756de64a617ac948105afdc12cd5b5
Ramazan İLTER <remzilter@gmail.com>
2022-11-03 03:52:21 +0300

    changes inheritance-protected solution for OK,OK

--- complete-oop-php-script/inheritance-protected-access-modifier.php
+++ complete-oop-php-script/inheritance-protected-access-modifier.php
@@ -4,6 +4,7 @@
 <body>
 
 <?php
+/*
 class Fruit {
   public $name;
   public $color;
@@ -26,9 +27,36 @@ class Strawberry extends Fruit {
 $strawberry = new Strawberry("Strawberry", "red");  // OK. __construct() is public
 $strawberry->message(); // OK. message() is public
 $strawberry->intro(); // ERROR. intro() is protected
+*/
 ?>
  
 </body>
 </html>
 
 <!-- Note: In the example above we see that if we try to call a protected method (intro()) from outside the class, we will receive an error. public methods will work fine! -->
+
+<?php
+class Fruit {
+  public $name;
+  public $color;
+  public function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color; 
+  }
+  protected function intro() {
+    echo "The fruit is {$this->name} and the color is {$this->color}."; 
+  }
+}
+
+class Strawberry extends Fruit {
+  public function message() {
+    echo "Am I a fruit or a berry? ";
+    // Call protected function from within derived class - OK 
+    $this -> intro();
+  }
+}
+$strawberry = new Strawberry("Strawberry", "red");  // OK. __construct() is public
+$strawberry->message(); // OK. message() is public and it calls intro() (which is protected) from within the derived class
+?>
+
+<!-- Note: The reason why everything worked fine in the above example is that we called the protected method (intro()) from inside the derived class. -->
