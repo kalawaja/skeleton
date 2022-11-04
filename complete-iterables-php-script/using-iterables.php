6f277edd7bb8b001e8afd5276dd55c7ab9f673e9
Ramazan İLTER <remzilter@gmail.com>
2022-11-04 23:02:38 +0300

    add complete using iterables

---
+++ complete-iterables-php-script/using-iterables.php
@@ -0,0 +1,82 @@
+
+<!-- with the function -->
+
+<!doctype html>
+<html>
+<body>
+
+<?php
+function printIterable(iterable $myIterable) {
+  foreach($myIterable as $item) {
+    echo $item;
+  }
+}
+
+$arr = ["a", "b", "c"];
+printIterable($arr);
+?>
+
+</body>
+</html>
+
+<!-- with the return -->
+
+<?php
+function getIterable():iterable {
+  return ["a", "b", "c"];
+}
+
+$myIterable = getIterable();
+foreach($myIterable as $item) {
+  echo $item;
+}
+?>
+
+<!-- Implement the Iterator interface and use it as an iterable: -->
+
+<?php
+/*
+// Create an Iterator
+class MyIterator implements Iterator {
+  private $items = [];
+  private $pointer = 0;
+
+  public function __construct($items) {
+    // array_values() makes sure that the keys are numbers
+    $this->items = array_values($items);
+  }
+
+  public function current() {
+    return $this->items[$this->pointer];
+  }
+
+  public function key() {
+    return $this->pointer;
+  }
+
+  public function next() {
+    $this->pointer++;
+  }
+
+  public function rewind() {
+    $this->pointer = 0;
+  }
+
+  public function valid() {
+    // count() indicates how many items are in the list
+    return $this->pointer < count($this->items);
+  }
+}
+
+// A function that uses iterables
+function printIterable(iterable $myIterable) {
+  foreach($myIterable as $item) {
+    echo $item;
+  }
+}
+
+// Use the iterator as an iterable
+$iterator = new MyIterator(["a", "b", "c"]);
+printIterable($iterator);
+*/
+?>
