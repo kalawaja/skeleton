5cba01382356f4a0d94148eaa9429949b24fa8f0
Ramazan İLTER <remzilter@gmail.com>
2022-11-07 16:23:10 +0300

    add get last inserted ID : PDO

--- complete-mysql-database-php-script/mysql-get-last-inserted-id/php-data-object.php
+++ complete-mysql-database-php-script/mysql-get-last-inserted-id/php-data-object.php
@@ -9,24 +9,14 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  // begin the transaction
-  $conn->beginTransaction();
-  // our SQL statements
-  $conn->exec("INSERT INTO MyGuests (firstname, lastname, email)
-  VALUES ('John', 'Doe', 'john@example.com')");
-  $conn->exec("INSERT INTO MyGuests (firstname, lastname, email)
-  VALUES ('Mary', 'Moe', 'mary@example.com')");
-  $conn->exec("INSERT INTO MyGuests (firstname, lastname, email)
-  VALUES ('Julie', 'Dooley', 'julie@example.com')");
-
-  // commit the transaction
-  $conn->commit();
-  echo "New records created successfully";
+  $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+  VALUES ('John', 'Doe', 'john@example.com')";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  $last_id = $conn->lastInsertId();
+  echo "New record created successfully. Last inserted ID is: " . $last_id;
 } catch(PDOException $e) {
-  // roll back the transaction if something failed
-  $conn->rollback();
-  echo "Error: " . $e->getMessage();
+  echo $sql . "<br>" . $e->getMessage();
 }
 
 $conn = null;
