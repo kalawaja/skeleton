7584cf97fa18549294afc59a0814ad211b7d4c2e
Ramazan İLTER <remzilter@gmail.com>
2022-11-07 13:37:33 +0300

    add close to connection with mysqli-oo

--- complete-mysql-database-php-script/mysql-connect/mysqli-object-oriented.php
+++ complete-mysql-database-php-script/mysql-connect/mysqli-object-oriented.php
@@ -12,4 +12,12 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+# Close the Connection
+// The connection will be closed automatically when the script ends. To close the connection before, use the following:
+
+/*
+MySQLi Object-Oriented:
+$conn->close();
+*/
 ?>
